25042663dc8459ba07e0b2d9ab15259d0a29f960
kentube2018@gmail.com <kentube2018@gmail.com>
2018-05-27 18:42:35 -0500

    SpyStore.DAL initial

---
+++ SpyStore.DAL/SpyStore.DAL/EF/MyExecutionStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace SpyStore.DAL.EF
+{
+    public class MyExecutionStrategy : ExecutionStrategy
+    {
+        public MyExecutionStrategy(
+            DbContext context) :
+            base(context, ExecutionStrategy.DefaultMaxRetryCount, ExecutionStrategy.DefaultMaxDelay)
+        {
+        }
+
+        public MyExecutionStrategy(DbContext context, int maxRetryCount, TimeSpan maxRetryDelay) :
+            base(context, maxRetryCount, maxRetryDelay)
+        {
+        }
+
+        protected override bool ShouldRetryOn(Exception exception)
+        {
+            return true;
+        }
+    }
+}
---
+++ SpyStore.DAL/SpyStore.DAL/EF/StoreContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using SpyStore.Models.Entities;
+
+namespace SpyStore.DAL.EF
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public StoreContext() { }
+
+        public StoreContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=SpyStore;Trusted_Connection=True;MultipleActiveResultSets=true;",
+                    //options => options.ExecutionStrategy(c => new MyExecutionStrategy(c))
+                    options => options.EnableRetryOnFailure()
+                    );
+            }
+        }
+    }
+}
